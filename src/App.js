690002717457f0ac88f0171ed006b67207ba3782
Christian Hubbard <chrishubbard92@hotmail.com>
2020-07-15 22:24:28 -0700

    Sending dummy data to palette component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import seedColors from "./seedColors";
 function App() {
   return (
     <div >
-     <Palette />
+     <Palette {...seedColors[4]}/>
     </div>
   );
 }
--- src/Palette.js
+++ src/Palette.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function Palette() {
     return (
-        <div>
-            
+        <div className='Palette'>
+            {/* {navbar goes here} */}
+            <div className='Palette-colors'>
+                {/* {footer will go here} */}
+            </div>
         </div>
     )
 }
