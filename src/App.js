8ff761fafe9bf719ec57e6f40f3b7444b06d49d9
Christian Hubbard <chrishubbard92@hotmail.com>
2020-07-15 22:20:11 -0700

    Creating seedColors.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Palette from './Palette';
-import './App.css';
+import seedColors from "./seedColors";
 
 function App() {
   return (
     <div >
-     
+     <Palette />
     </div>
   );
 }
