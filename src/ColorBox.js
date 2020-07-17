24ee32792607134727d4aa55cba705ad2459558a
Christian Hubbard <chrishubbard92@hotmail.com>
2020-07-16 20:22:07 -0700

    Adding CSS to colorboxes

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -3,10 +3,16 @@ import "./ColorBox.css"
 
 export default class ColorBox extends Component {
     render() {
+        const {name, background} = this.props
         return (
-            <div className="ColorBox" style={{background: this.props.background}}>
-                <span>MORE</span>
-                <span>{this.props.name}</span>
+            <div style={{background}} className="ColorBox">
+                <div className="copy-container">
+                    <div className ="box-content" >
+                        <span>{name}</span>
+                    </div>
+                    <button className="copy-button">Copy</button>
+                </div>
+                <span className="see-more">see more</span>
             </div>
         )
     }
