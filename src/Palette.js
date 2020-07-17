b195bb4aadbf9c4021d09b2595edfa9d6cd4110f
Christian Hubbard <chrishubbard92@hotmail.com>
2020-07-17 16:47:56 -0700

    Adding Styling to Slider

--- src/Palette.js
+++ src/Palette.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import ColorBox from "./ColorBox";
+import 'rc-slider/assets/index.css';
 import "./Palette.css"
 import Slider from "rc-slider";
-import 'rc-slider/assets/index.css';
 
  class Palette extends Component {
     constructor(props){
@@ -25,7 +25,9 @@ import 'rc-slider/assets/index.css';
         ))
         return (
             <div className='Palette'>
-                <Slider defaultValue={level} min={100} max={900} step={100} onAfterChange={this.changeLevel}/>
+                <div className='slider'>
+                    <Slider defaultValue={level} min={100} max={900} step={100} onAfterChange={this.changeLevel}/>
+                </div>
                 {/* {navbar goes here} */}
                 <div className='Palette-colors'>{colorBoxes}</div>
             </div>
