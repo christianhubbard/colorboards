3119e8cdf842f79609cdeabdbc2b0e6b95618ad4
Christian Hubbard <chrishubbard92@hotmail.com>
2020-07-16 21:13:23 -0700

    Adding copy to clipboard functionality

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -3,11 +3,24 @@ import {CopyToClipboard } from 'react-copy-to-clipboard';
 import "./ColorBox.css"
 
 export default class ColorBox extends Component {
+    constructor(props){
+        super(props);
+        this.state ={ copied: false };
+        this.changeCopyState = this.changeCopyState.bind(this);
+    }
+
+    changeCopyState(){
+        this.setState({copied: true}, () =>{
+            setTimeout(() => this.setState({copied: false}), 1500)
+        })
+    }
     render() {
         const {name, background} = this.props
+        const {copied} = this.state;
         return (
-            <CopyToClipboard text={background}>
-                <div style={{background}} className="ColorBox">
+            <CopyToClipboard text={background} onCopy={this.changeCopyState}>
+                <div style={{background}} className='ColorBox'>
+                    <div style={{background}} className={`copy-overlay ${copied && 'show'}`} />
                     <div className="copy-container">
                         <div className ="box-content">
                             <span>{name}</span>
