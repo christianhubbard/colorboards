a31d89c5e8e5738bb8e20eb522e101c347b2cf39
Christian Hubbard <chrishubbard92@hotmail.com>
2020-07-17 16:30:43 -0700

    Adding color helper js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import Palette from './Palette';
 import seedColors from "./seedColors";
+import {generatePalette} from "./colorHelpers"
 
 class App extends Component {
   render() {
     return (
       <div >
-        <Palette {...seedColors[4]}/>
+        <Palette palette={generatePalette(seedColors[4])}/>
       </div>
     );
   }
--- src/Palette.js
+++ src/Palette.js
@@ -4,8 +4,8 @@ import "./Palette.css"
 
  class Palette extends Component {
     render() {
-        const colorBoxes = this.props.colors.map(color => (
-            <ColorBox background={color.color} name={color.name}/>
+        const colorBoxes = this.props.palette.colors[500].map(color => (
+            <ColorBox background={color.hex} name={color.name}/>
         ))
         return (
             <div className='Palette'>
