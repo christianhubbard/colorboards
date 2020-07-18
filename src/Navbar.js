668ac6b92a898edd5645d8e6d78768dab08c854d
Christian Hubbard <chrishubbard92@hotmail.com>
2020-07-18 12:23:43 -0700

    Adding icon library to package.json

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import Select from "@material-ui/core/Select"
 import MenuItem from "@material-ui/core/MenuItem"
+import Snackbar from "@material-ui/core/Snackbar"
+import CloseIcon from "@material-ui/icons/Close"
+import IconButton from "@material-ui/core/IconButton"
 import Slider from "rc-slider";
 import 'rc-slider/assets/index.css';
 import './Navbar.css'
@@ -10,17 +13,23 @@ import './Navbar.css'
 export default class Navbar extends Component {
     constructor(props){
         super(props)
-        this.state = { format: "hex" }
+        this.state = { format: "hex",
+                        open: true, }
         this.handleChange = this.handleChange.bind(this);
+        this.closeSnackbar = this.closeSnackbar.bind(this);
     }
 
     handleChange(e){
         this.setState({format: e.target.value })
         this.props.handleChange(e.target.value);
     }
+
+    closeSnackbar(){
+        this.setState({open:false})
+    }
     render() {
         const {level, changeLevel } = this.props
-        const {format} = this.state;
+        const {format, open} = this.state;
         return (
             <header className='Navbar'>
                 <div className='logo'>
@@ -39,6 +48,17 @@ export default class Navbar extends Component {
                         <MenuItem value='rgba'> rgba - rgba(255,255,255,1.0)</MenuItem>
                     </Select>
                 </div>
+                <Snackbar 
+                anchorOrigin={{vertical: "bottom", horizontal:"left"}} 
+                open={open} autoHideDuration={3000} 
+                message={<span id="message-id"> Format Changed!</span>} 
+                ContentProps={{"aria-describedby": "message-id"}}
+                action={[
+                    <IconButton onClick={this.closeSnackbar}>
+                        <CloseIcon />
+                    </IconButton>
+                ]} 
+                />
             </header >
         )
     }
