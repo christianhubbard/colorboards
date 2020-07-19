cf5c9f8853e5b7c83ff0f1a4cec47103dc2e981d
Christian Hubbard <chrishubbard92@hotmail.com>
2020-07-19 11:24:21 -0700

    Finishing styling for MiniPalette and PaletteList

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -15,7 +15,11 @@ const styles = {
         }
     },
     colors: {
-        backgroundColor: "grey"
+        backgroundColor: "#dae1e4",
+        height: '150px',
+        width: '100%',
+        borderRadius: '5px',
+        overflow: 'hidden'
     },
     title: {
         display: "flex",
@@ -30,15 +34,29 @@ const styles = {
     emoji: {
         marginLeft: "0.5rem",
         fontSize: "1.5rem"
+    },
+    miniColor: {
+        height: '25%',
+        width: '20%',
+        display: 'inline-block',
+        margin: "0 auto",
+        position: 'relative',
+        marginBottom: '-4px'
     }
 };
 
 function MiniPalette(props) {
-    const { classes, paletteName, emoji} = props;
-    console.log(props)
+    const { classes, paletteName, emoji, colors} = props;
+    const miniColorBoxes = colors.map(color => (
+        <div className={classes.miniColor} style={{backgroundColor: color.color}} key={color.name}  />
+    ));
+
+
     return (
         <div className={classes.root}>
-            <div className={classes.colors} />
+            <div className={classes.colors} >
+                {miniColorBoxes}
+            </div>
             <h5 className={classes.title}>
                 {paletteName} <span className={classes.emoji}>{emoji}</span>
             </h5>
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -6,7 +6,7 @@ import { withStyles } from "@material-ui/styles"
 const styles= {
     root: {
         backgroundColor: 'blue',
-        height: "100%",
+        height: "100vh",
         display: 'flex',
         alignItems: 'flex-start',
         justifyContent: 'center'
