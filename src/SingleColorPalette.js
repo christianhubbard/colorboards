ce7757622471aecfa77844d05d8020f497dd21de
Christian Hubbard <chrishubbard92@hotmail.com>
2020-07-24 11:37:12 -0700

    Refactoring SingleColorPalette to work with navbar drop down

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -6,7 +6,10 @@ import './Palette.css'
 export default class SingleColorPalette extends Component {
     constructor(props){
         super(props);
-        this._shades = this.gatherShades(this.props.palette, this.props.colorId)
+        this._shades = this.gatherShades(this.props.palette, this.props.colorId);
+        this.state = {format: 'hex'};
+
+        this.changeFormat = this.changeFormat.bind(this);
     }
     gatherShades(palette, colorToFilterBy){
         //return all shades of given color
@@ -20,15 +23,23 @@ export default class SingleColorPalette extends Component {
 
 
         return shades.slice(1);
-    }
+    };
+    changeFormat(val){
+        this.setState({format: val})
+    };
     render() {
+        const {format} = this.state;
         const colorBoxes = this._shades.map( color => (
-            <ColorBox key={color.id} name={color.name} background={color.hex} showLink={false}/>
+            <ColorBox 
+            key={color.id} 
+            name={color.name} 
+            background={color[format]}
+            showLink={false} />
         ));
+        console.log(this._shades)
         return (
             <div className='Palette'>
-                <NavBar />
-                <h1>Single Color Palette </h1>
+                <NavBar handleChange={this.changeFormat} />
                 <div className='Palette-colors'>
                     {colorBoxes}
                 </div>
