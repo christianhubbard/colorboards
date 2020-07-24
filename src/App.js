6663cb846fb2f302b5ab0286a54e669083e8f0ae
Christian Hubbard <chrishubbard92@hotmail.com>
2020-07-23 22:24:07 -0700

    Adding Route for single color page

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
       <Switch>
         <Route exact path="/" render={(routeProps) => <PaletteList palettes={seedColors} {...routeProps}/>} />
         <Route exact path='/palette/:id' render={(routeProps) => <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} />} />
+        <Route exact path='/palette/:paletteId/:colorId' />
       </Switch>
       // <div >
       //   <Palette palette={generatePalette(seedColors[4])}/>
