21f500fea582eaddd42f409acc7ef9f79410a464
Christian Hubbard <chrishubbard92@hotmail.com>
2020-07-23 22:53:17 -0700

    Adding logic for gatherShades function

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
       <Switch>
         <Route exact path="/" render={(routeProps) => <PaletteList palettes={seedColors} {...routeProps}/>} />
         <Route exact path='/palette/:id' render={(routeProps) => <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} />} />
-        <Route exact path='/palette/:paletteId/:colorId' render={(routeProps) => <SingleColorPalette palette={generatePalette(this.findPalette(routeProps.match.params.paletteId)) } />} />
+        <Route exact path='/palette/:paletteId/:colorId' render={(routeProps) => <SingleColorPalette colorId={routeProps.match.params.colorId} palette={generatePalette(this.findPalette(routeProps.match.params.paletteId))  } />} />
       </Switch>
       // <div >
       //   <Palette palette={generatePalette(seedColors[4])}/>
--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -2,6 +2,23 @@ import React, { Component } from 'react'
 import NavBar from "./Navbar"
 
 export default class SingleColorPalette extends Component {
+    constructor(props){
+        super(props);
+        this._shades = this.gatherShades(this.props.palette, this.props.colorId)
+    }
+    gatherShades(palette, colorToFilterBy){
+        //return all shades of given color
+        let shades = []
+        let allColors = palette.colors;
+        for(let key in allColors){
+            shades = shades.concat(
+                allColors[key].filter(color => color.id === colorToFilterBy)
+            )
+        }
+
+
+        return shades.slice(1);
+    }
     render() {
         return (
             <div>
