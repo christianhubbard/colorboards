5c48c5b7c131dc10b11a3c986658cc6766fa142e
Christian Hubbard <chrishubbard92@hotmail.com>
2020-07-23 22:35:41 -0700

    Adding Link props to colorbox link

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -17,7 +17,7 @@ export default class ColorBox extends Component {
         })
     }
     render() {
-        const {name, background} = this.props
+        const {name, background, paletteId, id} = this.props
         const {copied} = this.state;
         return (
             <CopyToClipboard text={background} onCopy={this.changeCopyState}>
@@ -33,7 +33,7 @@ export default class ColorBox extends Component {
                         </div>
                         <button className="copy-button">Copy</button>
                     </div>
-                    <Link to='/' onClick={e => e.stopPropagation()}>
+                    <Link to={`/palette/${paletteId}/${id}`} onClick={e => e.stopPropagation()}>
                         <span className="see-more">More</span>
                     </Link>
                 </div>
