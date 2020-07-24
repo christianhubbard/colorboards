863ecf025080d9c4a8cb17a9ee564400818539c8
Christian Hubbard <chrishubbard92@hotmail.com>
2020-07-23 22:41:29 -0700

    Adding navbar to SingleColorPalette

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import NavBar from "./Navbar"
 
 export default class SingleColorPalette extends Component {
     render() {
         return (
             <div>
+                <NavBar />
                 <h1>Single Color Palette </h1>
             </div>
         )
