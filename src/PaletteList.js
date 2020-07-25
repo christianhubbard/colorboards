72c001ae865ed44136c782beb7804809f2a95c64
Christian Hubbard <chrishubbard92@hotmail.com>
2020-07-25 11:52:50 -0700

    Refactoring paletteList to seperate styles file

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,37 +1,10 @@
 import React, { Component } from 'react'
-import { Link } from "react-router-dom"
 import MiniPalette from "./MiniPalette"
+
+import styles from './styles/PaletteListStyles'
 import { withStyles } from "@material-ui/styles"
 
-const styles= {
-    root: {
-        backgroundColor: 'blue',
-        height: "100vh",
-        display: 'flex',
-        alignItems: 'flex-start',
-        justifyContent: 'center'
-    },
-    container: {
-        width: '50%',
-        display: 'flex',
-        alignItems: 'flex-start',
-        flexDirection: 'column',
-        flexWrap: 'wrap',
-    },
-    nav: {
-        display: 'flex',
-        width: '100%',
-        justifyContent: 'space-between',
-        color: 'white'
-    },
-    palettes: {
-        boxSizing: 'broder-box',
-        width: '100%',
-        display: 'grid',
-        gridTemplateColumns: 'repeat(3, 30%)',
-        gridGap: '5%'
-    }
-};
+
  class PaletteList extends Component {
      goToPalette(id){
          this.props.history.push(`/palette/${id}`)
