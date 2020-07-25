83717aec07bd6d8a5d750c52e63ec482d7f1af0d
Christian Hubbard <chrishubbard92@hotmail.com>
2020-07-25 11:49:45 -0700

    Refactoring palette footer to use WithStyles

--- src/PaletteFooter.js
+++ src/PaletteFooter.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import './Palette.css'
 
-export default function PaletteFooter(props) {
-    const {paletteName, emoji} = props;
+import styles from './styles/FooterStyles'
+import {withStyles} from '@material-ui/styles';
+
+
+function PaletteFooter(props) {
+    const {paletteName, emoji, classes} = props;
     return (
-        <footer className='Palette-footer'>
+        <footer className={classes.PaletteFooter}>
             {paletteName}
-            <span className='emoji'>{emoji}</span>    
+            <span className={classes.emoji}>{emoji}</span>    
         </footer>
     )
 }
+
+export default withStyles(styles)(PaletteFooter);
---
+++ src/styles/FooterStyles.js
@@ -0,0 +1,14 @@
+export default {
+    PaletteFooter: {
+        backgroundColor: `white`,
+        height: `5vh`,
+        display: `flex`,
+        justifyContent: `flex-end`,
+        alignItems: `center`,
+        fontWeight: `bold`,
+    },
+    emoji: {
+        fontsize: `1.5rem`,
+        margin: `0 1rem`,
+    }
+}
