acaf96beaf717ec58dc3da8ba0f71d267ef1328f
Christian Hubbard <chrishubbard92@hotmail.com>
2020-07-25 11:46:25 -0700

    Refactoring MiniPaletteStyles into seperate file

--- src/Palette.js
+++ src/Palette.js
@@ -1,20 +1,13 @@
 import React, { Component } from 'react'
 import ColorBox from "./ColorBox";
-import "./Palette.css"
+
 import Navbar from './Navbar'
 import PaletteFooter from './PaletteFooter';
+
+import styles from './styles/PaletteStyles'
 import {withStyles} from "@material-ui/styles"
 
-const styles = {
-    Palette:{
-        height:`100vh`,
-        display: `flex`,
-        flexDirection: `column`,
-    },
-    colors: {
-        height: `90%`,
-    }
-}
+
  class Palette extends Component {
     constructor(props){
         super(props)
---
+++ src/styles/PaletteStyles.js
@@ -0,0 +1,44 @@
+
+const styles = {
+    Palette:{
+        height:`100vh`,
+        display: `flex`,
+        flexDirection: `column`,
+    },
+    colors: {
+        height: `90%`,
+    },
+    goBack: {
+        width: "20%",
+        height: '50%',
+        margin: "0 auto",
+        display: "inline-block",
+        position: "relative",
+        cursor: "pointer",
+        marginBottom: "-4.5px",
+        opacity: 1,
+        backgroundColor: `black`,
+        '& a': {
+            width: "100px",
+            height: "30px",
+            position: "absolute",
+            display: "inline-block",
+            top: "50%",
+            left: "50%",
+            marginLeft: "-50px",
+            marginTop: "-15px",
+            textAlign:"center",
+            outline: "none",
+            background: "rgba(255,255,255,0.3)",
+            fontSize: "1rem",
+            lineHeight: "30px",
+            color: "white",
+            textTransform: "uppercase",
+            border:"none",
+            textDecoration: "none",
+            cursor: 'pointer',
+        }
+    },
+}
+
+export default styles
