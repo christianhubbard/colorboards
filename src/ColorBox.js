033164d8c7438133af03f910c7872e7127d673ee
Christian Hubbard <chrishubbard92@hotmail.com>
2020-07-25 10:32:20 -0700

    Changing text color based on luminance values

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import {CopyToClipboard } from 'react-copy-to-clipboard';
 import "./ColorBox.css"
+import chroma from 'chroma-js'
 
 import {Link} from 'react-router-dom'
 
@@ -19,22 +20,24 @@ export default class ColorBox extends Component {
     render() {
         const {name, background, paletteId, id, showLink} = this.props
         const {copied} = this.state;
+        const isDarkColor = chroma(background).luminance() <= 0.08;
+        const isLightColor = chroma(background).luminance() >= 0.62;
         return (
             <CopyToClipboard text={background} onCopy={this.changeCopyState}>
                 <div style={{background}} className='ColorBox'>
                     <div style={{background}} className={`copy-overlay ${copied && 'show'}`} />
                     <div className={`copy-msg ${copied && 'show'}`}>
                         <h1>Copied!</h1>
-                        <p>{background}</p>
+                        <p className={isLightColor && "dark-text"}>{background}</p>
                     </div>
                     <div className="copy-container">
                         <div className ="box-content">
-                            <span>{name}</span>
+                            <span className={isDarkColor && 'light-text'}>{name}</span>
                         </div>
-                        <button className="copy-button">Copy</button>
+                        <button className={`copy-button ${isLightColor && "dark-text"}`}>Copy</button>
                     </div>
                     {showLink && <Link to={`/palette/${paletteId}/${id}`} onClick={e => e.stopPropagation()}>
-                        <span className="see-more">More</span>
+                        <span className={`see-more ${isLightColor && "dark-text"}`}>MORE</span>
                     </Link>}
                 </div>
             </CopyToClipboard>
