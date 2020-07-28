29917f10fad49b1a781b8c19c6d3bac851d4c9de
Christian Hubbard <chrishubbard92@hotmail.com>
2020-07-27 20:36:35 -0700

    Adding Route for new palette form

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
     return (
       <Switch>
         <Route exact path="/" render={(routeProps) => <PaletteList palettes={seedColors} {...routeProps}/>} />
+        <Route exact path="/palette/new" render={() => <h1>new Pallet Form!</h1>} />
         <Route exact path='/palette/:id' render={(routeProps) => <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} />} />
         <Route exact path='/palette/:paletteId/:colorId' render={(routeProps) => <SingleColorPalette colorId={routeProps.match.params.colorId} palette={generatePalette(this.findPalette(routeProps.match.params.paletteId))  } />} />
       </Switch>
