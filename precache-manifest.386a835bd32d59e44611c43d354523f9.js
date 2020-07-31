309f4980971295d9e245af01eb6d085e4153ec47
Christian Hubbard <chrishubbard92@hotmail.com>
2020-07-31 11:58:57 -0700

    Updates

---
+++ precache-manifest.386a835bd32d59e44611c43d354523f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f00288d062a36c08101bfa9222a1151",
+    "url": "/colorboards/index.html"
+  },
+  {
+    "revision": "837fdaad6e309d132737",
+    "url": "/colorboards/static/css/2.0da342d8.chunk.css"
+  },
+  {
+    "revision": "de65da51dcfb9dbed3e7",
+    "url": "/colorboards/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "837fdaad6e309d132737",
+    "url": "/colorboards/static/js/2.78260419.chunk.js"
+  },
+  {
+    "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
+    "url": "/colorboards/static/js/2.78260419.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de65da51dcfb9dbed3e7",
+    "url": "/colorboards/static/js/main.3a328ff1.chunk.js"
+  },
+  {
+    "revision": "a33dd08eb6e40f12028a",
+    "url": "/colorboards/static/js/runtime-main.c19dd06e.js"
+  }
+]);
