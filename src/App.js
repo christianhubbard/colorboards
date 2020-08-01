992a14cf7272bcb7451d529f92a914a39b70e98a
Christian Hubbard <chrishubbard92@hotmail.com>
2020-07-31 19:17:21 -0700

    Starting responsive styles in colorbox

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ class App extends Component {
         <Route exact path="/palette/new" render={(routeProps) => <NewPaletteForm savePalette={this.savePalette} palettes={this.state.palettes} {...routeProps} />} />
         <Route exact path='/palette/:id' render={(routeProps) => <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} />} />
         <Route exact path='/palette/:paletteId/:colorId' render={(routeProps) => <SingleColorPalette colorId={routeProps.match.params.colorId} palette={generatePalette(this.findPalette(routeProps.match.params.paletteId))  } />} />
-        <Route path="/" render={(deletePalette, routeProps) => <PaletteList palettes={this.state.palettes} deletePalette={this.deletePalette} {...routeProps}/>} />
+        <Route path="/" render={(routeProps) => <PaletteList palettes={this.state.palettes} deletePalette={this.deletePalette} {...routeProps}/>} />
       </Switch>
     );
   }
--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -1,4 +1,5 @@
 import chroma from 'chroma-js'
+import sizes from './sizes'
 
 const styles = {
     ColorBox: {
@@ -11,6 +12,18 @@ const styles = {
         marginBottom: "-4.5px",
         "&:hover button": {
             opacity: 1
+        },
+        [sizes.down('lg')]: {
+            width: `25%`,
+            height: props => props.showingFullPalette ? "20%" : '50%',
+        },
+        [sizes.down('md')]: {
+            width: `50%`,
+            height: props => props.showingFullPalette ? "10%" : '40%',
+        },
+        [sizes.down('xs')]: {
+            width: '100%',
+            height: props => props.showingFullPalette ? "5%" : '10%',
         }
     },
     copyText: {
