6edd6d241c5ed7c1c7c32e96e61b530019499e46
Christian Hubbard <chrishubbard92@hotmail.com>
2020-07-31 22:44:11 -0700

    Making PaletteList responsive

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,3 +1,5 @@
+import sizes from './sizes'
+
 export default {
     root: {
         backgroundColor: 'blue',
@@ -12,6 +14,12 @@ export default {
         alignItems: 'flex-start',
         flexDirection: 'column',
         flexWrap: 'wrap',
+        [sizes.down('xl')] : {
+            width: '80%'
+        },
+        [sizes.down('xs')] : {
+            width: '75%'
+        }
     },
     nav: {
         display: 'flex',
@@ -28,6 +36,13 @@ export default {
         width: '100%',
         display: 'grid',
         gridTemplateColumns: 'repeat(3, 30%)',
-        gridGap: '5%'
+        gridGap: '1.5rem',
+        [sizes.down('md')] : {
+            gridTemplateColumns: 'repeat(2, 50%)'
+        },
+        [sizes.down('xs')] : {
+            gridTemplateColumns: 'repeat(1, 100%)',
+            gridGap: '1rem',
+        },
     }
 };
--- src/styles/sizes.js
+++ src/styles/sizes.js
@@ -10,6 +10,7 @@ export default {
             sm: "767.98px",
             md: "991.98px",
             lg: "1199.98px",
+            xl: '1600px'
         }
         return `@media (max-width: ${sizes[size]})`
     }
