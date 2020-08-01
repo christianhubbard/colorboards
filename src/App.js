f5857aa0a1549f04ea29c74aaeac2c4c2f61c686
Christian Hubbard <chrishubbard92@hotmail.com>
2020-07-31 17:51:08 -0700

    Storing palettes in local storage

--- src/App.js
+++ src/App.js
@@ -10,9 +10,10 @@ import NewPaletteForm from "./NewPaletteForm";
 class App extends Component {
   constructor(props){
     super(props)
+    const savedPalettes = JSON.parse(window.localStorage.getItem("palettes"))
     this.state = {
-      palettes: seedColors
-    }
+      palettes: savedPalettes || seedColors
+    };
 
     this.savePalette = this.savePalette.bind(this);
     this.findPalette = this.findPalette.bind(this);
@@ -25,7 +26,11 @@ class App extends Component {
   }
 
   savePalette(newPalette){
-    this.setState({palettes: [...this.state.palettes, newPalette] })
+    this.setState({palettes: [...this.state.palettes, newPalette] }, this.syncLocalStorage);
+  }
+
+  syncLocalStorage(){
+    window.localStorage.setItem("palettes", JSON.stringify(this.state.palettes))
   }
 
   render() {
