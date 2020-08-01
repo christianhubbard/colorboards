2b744c8032a7e4aed36e61e3ff24bcd3642e59ab
Christian Hubbard <chrishubbard92@hotmail.com>
2020-07-31 17:16:29 -0700

    Refactoring styles into seperate JS files

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -3,40 +3,7 @@ import {withStyles} from '@material-ui/styles'
 import DeleteIcon from "@material-ui/icons/Delete"
 import {SortableElement} from "react-sortable-hoc"
 
-const styles = {
-    root: {
-        width: "20%",
-        height: "25%",
-        margin: "0 auto",
-        display: "inline-block",
-        position: "relative",
-        cursor: "pointer",
-        marginBottom: "-3.5px",
-        "&:hover svg" : {
-            color: "white",
-            transform: "scale(1.25)"
-        }
-    },
-    boxContent: {
-        position: "absolute",
-        width: "100%",
-        left: "0px",
-        bottom: "0px",
-        padding: "10px",
-        color: "rgba(0,0,0,0.5)",
-        letterSpacing: "1px",
-        textTransform: "uppercase",
-        fontSize: "12px",
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: 'flex-end',
-
-    },
-    deleteIcon: {
-        transition: "all 0.3s ease-in-out",
-    }
-}
-
+import styles from './styles/DraggableColorBoxStyles'
 
 
 const DraggableColorBox = SortableElement((props) => {
