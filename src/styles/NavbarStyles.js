a881c2bfdc98d5c89e653b62dad7144dc12e71c6
Christian Hubbard <chrishubbard92@hotmail.com>
2020-07-31 19:45:05 -0700

    Making form and navbar form responsive

--- src/styles/NavbarStyles.js
+++ src/styles/NavbarStyles.js
@@ -1,3 +1,5 @@
+import sizes from './sizes'
+
 export default {
     Navbar: {
         display: `flex`,
@@ -17,7 +19,10 @@ export default {
         "& a": {
             textDecoration: `none`,
             color: `black`,
-        }
+        },
+        [sizes.down('xs')] : {
+            display: `none`,
+        },
     },
     slider: {
         width: `340px`,
@@ -38,6 +43,9 @@ export default {
             height: `13px`,
             marginLeft: `-7px`,
             marginTop: `-3px`,
+        },
+        [sizes.down('sm')]: {
+            width: "150px"
         }
     },
     selectContainer: {
--- src/styles/PaletteFormNavStyles.js
+++ src/styles/PaletteFormNavStyles.js
@@ -1,4 +1,5 @@
 import {DRAWER_WIDTH} from "../Constants.js";
+import sizes from './sizes'
 
 const drawerWidth = DRAWER_WIDTH;
 
@@ -35,10 +36,17 @@ const styles =  theme => ({
         marginRight: '1rem',
         "& a":{
             textDecoration: 'none'
-        }
+        },
+        [sizes.down('xs')] : {
+          marginRight: '0.5rem',
+        },
       },
       button: {
         margin: '0 0.5rem',
+        [sizes.down('xs')] : {
+          margin: `0 0.2rem`,
+          padding: '0.3rem',
+        },
       },
 
 });
