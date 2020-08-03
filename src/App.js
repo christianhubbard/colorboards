4291a6aa257b49814ec3865c3c3089d7d49b6947
Christian Hubbard <chrishubbard92@hotmail.com>
2020-08-02 21:02:15 -0700

    Adding exact route and 404 route

--- src/App.js
+++ src/App.js
@@ -50,6 +50,7 @@ class App extends Component {
         <TransitionGroup>
           <CSSTransition key={location.key} classNames='page' timeout={500}>
             <Switch location={location}>
+              <Route exact path="/" render={(routeProps) => <Page><PaletteList palettes={this.state.palettes} deletePalette={this.deletePalette} {...routeProps}/></Page>} />
               <Route exact path="/palette/new" render={(routeProps) => <Page><NewPaletteForm savePalette={this.savePalette} palettes={this.state.palettes} {...routeProps} /></Page>} />
               <Route exact path='/palette/:id' render={(routeProps) => <Page><Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} /></Page>} />
               <Route exact path='/palette/:paletteId/:colorId' render={(routeProps) => <Page><SingleColorPalette colorId={routeProps.match.params.colorId} palette={generatePalette(this.findPalette(routeProps.match.params.paletteId))  } /></Page>} />
